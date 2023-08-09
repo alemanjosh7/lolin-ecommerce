017bd0acab978f5a4a6e80a9c150e92317a1cd1c
alemanjosh7 <josuealeman2003@gmail.com>
2023-08-09 02:00:07 -0600

    Authentication finished using Clerk Library

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  SignOutButton
+} from "@clerk/nextjs";
+
+const SetupPage = ( )=> {
   return (
-    <h1>Hello</h1>
+    <div className="p-4 flex justify-end"> 
+      <div className="mr-4">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+      <div className="mt-1">
+        <SignOutButton/>
+      </div>
+    </div>
   )
 }
+
+export default SetupPage;
